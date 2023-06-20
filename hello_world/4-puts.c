819886cb71ecb21dd4ae1b49faa91583e849b8c8
Samcalde <s.calderon0624@gmail.com>
2023-06-20 14:47:27 -0700

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 /**
- *Programprints a sentence
- *@main is the general program
- *@return works as the indicator that the program works 
- */
+*Programprints a sentence
+*@main is the general program
+*@return works as the indicator that the program works 
+*/
 #include <stdio.h>
 int main(void)
 {putchar('"');
