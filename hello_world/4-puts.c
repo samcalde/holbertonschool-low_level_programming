b52be8dcc3a2f0212e78e41a46161fe25ecc74df
Samcalde <s.calderon0624@gmail.com>
2023-06-20 14:38:16 -0700

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main() {
-  putchar('"');
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+int main(void)
+{putchar('"');
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
