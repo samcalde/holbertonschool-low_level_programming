77b0931d806992e2f5f07c690c748acf95f3e37f
Samcalde <s.calderon0624@gmail.com>
2023-06-21 10:40:23 -0700

    Task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 122; a > 96; a--)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
