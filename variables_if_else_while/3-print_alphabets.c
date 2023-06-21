7bbc291cb5b548b379a6303b6a716fe42eddb6dc
Samcalde <s.calderon0624@gmail.com>
2023-06-21 09:45:56 -0700

    Task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 97; a < 123; a++)
+{
+putchar(a);
+}
+for (a = 65; a < 91; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
