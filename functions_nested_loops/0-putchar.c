f28992405abf0b17a00d5a6e188cafc7b093473e
Samcalde <s.calderon0624@gmail.com>
2023-06-21 13:54:52 -0700

    Task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+char word[] = "_putchar";
+for (i = 0; i < 8; i++)
+{
+_putchar(word[i]);
+}
+ _putchar('\n');
+return (0);
+}
