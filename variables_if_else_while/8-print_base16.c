4a572f37ff3559f37bf36e4797aa5c97d9154f9e
Samcalde <s.calderon0624@gmail.com>
2023-06-21 11:13:06 -0700

    Task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+}
+for (a = 65; a < 71; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
