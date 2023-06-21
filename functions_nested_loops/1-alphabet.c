9574cca9050f7f37830fe95a39b90a5e1e6409c1
Samcalde <s.calderon0624@gmail.com>
2023-06-21 14:21:52 -0700

    Task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,12 +4,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int alphabet(void)
 {
 int i;
 for (i = 97; i < 123; i++)
 {
 _putchar(i);
 }
+_putchar('\n');
 return (0);
 }
--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -6,6 +6,11 @@
  */
 int main(void)
 {
-print_alphabet();
+int i;
+for (i = 97; i < 123; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 return (0);
 }
