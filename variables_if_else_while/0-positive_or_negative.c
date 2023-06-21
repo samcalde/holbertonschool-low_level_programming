3c6767a41a9dfd380ee6fe2bdf8f55d977d5926f
Samcalde <s.calderon0624@gmail.com>
2023-06-20 20:43:25 -0700

    test

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 if (n>0)
-{printf (%n is positive)}
-else if (n<0)
-{printf (%n is negtivve)}
-else
+{printf (%n is positive)
+} else if (n<0)
+{printf (%n is negtivve)
+} else
 {printf (%n is zero)}
