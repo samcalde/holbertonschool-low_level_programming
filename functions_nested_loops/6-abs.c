66f9e7ce41fa3f20fd9cf4221734da775713e7ff
Samcalde <s.calderon0624@gmail.com>
2023-06-22 09:27:16 -0700

    test

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - prints absolute value
+ * @n: number to be converted
+ *
+ * Return: absolute value of number.
+ */
+int _abs(int n)
+{
+int r;
+if (n < 0)
+{
+r = (-n);
+}
+return (r);
+}
