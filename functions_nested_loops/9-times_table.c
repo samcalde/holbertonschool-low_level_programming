b5788422156c0c47f957d304c53ff487e0a0fb6e
Samcalde <s.calderon0624@gmail.com>
2023-06-23 07:23:22 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,19 +6,29 @@
  */
 void times_table(void)
 {
-int i;
-int j;
+int n1;
+int n2;
 int r;
-for (i = 0; i < 10; i++)
+for (n1 = 0; n1 < 10; n1++)
 {
-for (j = 0; j < 10; j++)
+_putchar(48);
+for (n2 = 1; n1 < 10; n2++)
 {
-r = i * j;
-_putchar(r + '0');
+r = n1 * n2;
 _putchar(',');
+if (r < 10)
+{
 _putchar(' ');
+_putchar(' ');
+_putchar(r + 48);
 }
-_putchar('\n');
+if (r > 10)
+{
+_putchar(' ');
+_putchar(48 + (r / 10));
+_putchar(48 + (r % 10));
+}
+}
 }
 return;
 }
