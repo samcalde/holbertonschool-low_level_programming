a349aed0b90c3d12e26d83e6ee774118a508af07
Samcalde <s.calderon0624@gmail.com>
2023-06-23 08:49:13 -0700

    test

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,21 +8,19 @@
  */
 void print_to_98(int n)
 {
-if (n < 98)
-{
-for (n = n; n < 98; n++)
-{
-printf("%d, ", n);
-}
-}
-if (n > 98)
-{
-for (n = n; n > 98; n--)
-{
-printf("%d ", n);
-}
-}
-printf("98");
-_putchar('\n');
-return;
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	if (n > 98)
+	{
+		for (n = n; n > 98; n--)
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("98");
 }
