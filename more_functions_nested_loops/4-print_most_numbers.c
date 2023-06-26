2942560c4739870eb4f195a9b3a981e89290b287
Samcalde <s.calderon0624@gmail.com>
2023-06-26 08:40:47 -0700

    test

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+if (i != 50 && i != 52)
+{
+_putchar(i);
+}
+}
+return;
+}
