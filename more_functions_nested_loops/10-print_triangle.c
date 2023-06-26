a68c15dbb3baae84b35db19b3ecfbc030eb62618
Samcalde <s.calderon0624@gmail.com>
2023-06-26 16:15:27 -0700

    teest

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - Print a triangle
+ * @s: size
+ *
+ * Return: void.
+ */
+void print_triangle(int s)
+{
+int t;
+int b;
+if (s > 0)
+{
+for (t = 0; t < s; t++)
+{
+for (b = (s - 1); b > 0; b--)
+{
+_putchar(' ');
+}
+for (b = (t + 1); b > 0; b--)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+return;
+}
