4dc8a43a20a5ff18db67057c8ff17622fb3c08f4
Samcalde <s.calderon0624@gmail.com>
2023-06-27 18:55:29 -0700

    test

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints in reverse
+ * @s: string
+ *
+ * Return: Void
+ */
+void print_rev(char *s)
+{
+int i;
+int r = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+r = r + 1;
+}
+for (i = r; i < 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+return;
+}
