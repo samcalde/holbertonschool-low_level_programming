32081297fc4ab2224d6b10049c0a0d47a641d197
Samcalde <s.calderon0624@gmail.com>
2023-06-28 07:56:26 -0700

    test 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,10 @@
 char *_strcpy(char *dest, char *src)
 {
 int i;
-int l;
 for (i = 0; src[i] != '\0'; i++)
 {
-l = l + 1;
 dest[i] = src[i];
 }
-dest[l] = '\0';
+dest[i + 1] = '\0';
 return (dest);
 }
