d8a9b60bd2a2112b9f7428956fef51d2f62e2459
Samcalde <s.calderon0624@gmail.com>
2023-07-07 21:49:43 -0700

    task 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -8,14 +8,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int check = 'f';
-
 	if (*s == '\0')
 	{
-		check = 't';
 		return;
 	}
-	if (check == 't')
-		_putchar(*s);
 	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
