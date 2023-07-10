303b48f0e39394d3759cca81a1c5206e52039288
Samcalde <s.calderon0624@gmail.com>
2023-07-09 22:26:26 -0700

    task 3.1

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -3,16 +3,16 @@
 
 /**
  * factorial - self explainatory
+ * @n: number
  *
  * Return: factorial of a number.
  */
 
-int factorial(int n) {
-    if (n < 0) {
-        return -1;
-    }
-    if (n == 0) {
-        return 1;
-    }
-    return n * factorial(n - 1);
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 0)
+return (1);
+return (n * factorial(n - 1));
 }
