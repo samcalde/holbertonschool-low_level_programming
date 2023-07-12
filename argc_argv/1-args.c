45fcdbf304edc93161451cc926c30f5d6cbcdcfd
Samcalde <s.calderon0624@gmail.com>
2023-07-11 23:16:16 -0700

    task 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,8 +7,8 @@
  *
  * Return: void
  */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	printf("%s", argv[0]);
+	printf("%d\n", (argc - 1));
 	return (0);
 }
