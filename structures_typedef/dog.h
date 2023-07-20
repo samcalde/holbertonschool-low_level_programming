9fe3d7d6a5f0f7591eccb011a9a2db690cf0085a
Samcalde <s.calderon0624@gmail.com>
2023-07-19 20:41:54 -0700

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - new type
+ * @name: name
+ * @age: age
+ * @owner: Owner
+ *
+ * Description: Create a new dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
