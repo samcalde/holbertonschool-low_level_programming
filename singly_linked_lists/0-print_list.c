2546222b15dd1aec4776a63cb10642a3e148ea03
Samcalde <s.calderon0624@gmail.com>
2023-08-02 21:48:47 -0700

    task 0.3

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,18 +14,18 @@ size_t print_list(const list_t *h)
 list_t *nextelement = (list_t *)h;
 int nodes = 0;
 
- while (nextelement != NULL)
+while (nextelement != NULL)
+{
+	if (nextelement->str == NULL)
 	{
-		if (nextelement->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%d] %s\n", nextelement->len, nextelement->str);
-		}
-		nodes++;
-		nextelement =  nextelement->next;
+		printf("[0] (nil)\n");
 	}
-	return (nodes);
+	else
+	{
+		printf("[%d] %s\n", nextelement->len, nextelement->str);
+	}
+	nodes++;
+	nextelement =  nextelement->next;
+}
+return (nodes);
 }
