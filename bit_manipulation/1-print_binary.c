75d6bbabccf962e4be52eac24025e09384a6910b
Samcalde <s.calderon0624@gmail.com>
2023-08-06 13:53:08 -0700

    task 1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -10,27 +10,26 @@
  */
 void print_binary(unsigned long int n)
 {
-    int bits = sizeof(unsigned long int) * 8;
-    int ceroi = 1;
-    int i;
+	int bits = sizeof(unsigned long int) * 8;
+	int ceroi = 1;
+	int i;
 
-    for (i = bits - 1; i >= 0; i--)
+	for (i = bits - 1; i >= 0; i--)
+	{
+		unsigned long int mask = 1UL << i;
+
+		if ((n & mask) != 0)
 		{
-			unsigned long int mask = 1UL << i;
-
-			if ((n & mask) != 0)
-				{
-					_putchar('1');
-					ceroi = 0;
-				}
-			else if (!ceroi)
-				{
-					_putchar('0');
-				}
+			_putchar('1');
+			ceroi = 0;
 		}
-
-    if (ceroi)
+		else if (!ceroi)
 		{
 			_putchar('0');
 		}
+	}
+	if (ceroi)
+	{
+		_putchar('0');
+	}
 }
