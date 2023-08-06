854a8c074e914cb8d84546bc566cfbbfa79a75f6
Samcalde <s.calderon0624@gmail.com>
2023-08-06 13:49:31 -0700

    task 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+    int bits = sizeof(unsigned long int) * 8;
+    int ceroi = 1;
+    int i;
+
+    for (i = bits - 1; i >= 0; i--)
+		{
+			unsigned long int mask = 1UL << i;
+
+			if ((n & mask) != 0)
+				{
+					_putchar('1');
+					ceroi = 0;
+				}
+			else if (!ceroi)
+				{
+					_putchar('0');
+				}
+		}
+
+    if (ceroi)
+		{
+			_putchar('0');
+		}
+}
