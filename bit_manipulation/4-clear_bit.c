9c898187eff090322d7f9089777cb947661555a5
samcalde <129321248+samcalde@users.noreply.github.com>
2023-08-06 18:33:38 -0500

    Task 4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @index: index
+ * @n: number
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+    unsigned long int mask;
+    
+    if (index >= sizeof(unsigned long int) * 8)
+    {
+        return (-1);
+    }
+    mask = 1UL << index;
+    mask = ~mask; 
+    *n &= mask; 
+    return (1); 
+}
