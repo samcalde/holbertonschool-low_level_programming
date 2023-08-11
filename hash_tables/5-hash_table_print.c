9914734b40559973f1e74671c2c5ff1cd900b205
Samuel <samuel@iMac-de-Andres.local>
2023-08-11 18:46:57 -0500

    task 5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *currentnode;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		currentnode = ht->array[i];
+		while (currentnode != NULL)
+		{
+			if ((i + 1) != ht->size)
+				printf("'%s': '%s', ", currentnode->key, currentnode->value);
+			else
+				printf("'%s': '%s'", currentnode->key, currentnode->value);
+			currentnode = currentnode->next;
+		}
+	}
+	printf("}\n");
+}
