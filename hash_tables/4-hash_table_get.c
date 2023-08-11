e1a5ef7d69987ed03f600b1e9b98ff9bfa81144c
Samuel <samuel@iMac-de-Andres.local>
2023-08-11 16:57:38 -0500

    task 4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @key: key
+ * @ht: hash table
+ *
+ * Return: value associated with the element, or NULL if key wasn't found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	char *value;
+	unsigned long int index;
+	hash_node_t *currentnode = NULL;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	currentnode = ht->array[index];
+	while (*currentnode->key != *key)
+	{
+		if (currentnode->next == NULL)
+			return (NULL);
+			currentnode = currentnode->next;
+	}
+
+	value = currentnode->value;
+	return (value);
+}
