7bb96a78660035e130c55f8da794a60407eacbab
Samuel <samuel@iMac-de-Andres.local>
2023-08-11 16:29:33 -0500

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,18 +13,22 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	char *str = NULL;
 	unsigned long int index;
 	hash_node_t *currentnode = NULL;
 	hash_node_t *newnode = NULL;
 
-	strcpy(str, value);
 	newnode = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
 		return (0);
 
-	newnode->key = (char *)key;
-	newnode->value = str;
+	newnode->key = strdup(key);
+	if (newnode->key == NULL)
+	{
+		free(newnode);
+		return (0);
+	}
+
+	newnode->value = strdup(value);
 	index = key_index((const unsigned char *)key, ht->size);
 
 	currentnode = ht->array[index];
