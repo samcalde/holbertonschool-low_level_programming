52a06485b6d42f5b69477e840191cf58b8a0ea39
Samuel <samuel@iMac-de-Andres.local>
2023-08-11 20:06:32 -0500

    task 3 correction 5

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -34,7 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	while (currentnode != NULL)
 	{
-		if(currentnode->key == key)
+		if (strcmp(currentnode->key, key) == 0)
 		{
 			currentnode->value = strdup(value);
 			return (1);
