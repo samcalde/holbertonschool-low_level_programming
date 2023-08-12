2ac81a497759aa864b39bafb38e7522b35b23368
Samuel <samuel@iMac-de-Andres.local>
2023-08-11 19:38:39 -0500

    task 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *currentnode, *temp;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		currentnode = ht->array[i];
+		while (currentnode != NULL)
+		{
+			temp = currentnode;
+			free(currentnode->value);
+			free(currentnode);
+			currentnode = temp->next;
+		}
+	}
+	free(temp->value);
+	free(temp);
+}
