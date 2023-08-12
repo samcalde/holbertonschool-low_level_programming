e0a401ad02b24f4b39e87c193134a50ac5c38482
Samuel <samuel@iMac-de-Andres.local>
2023-08-11 19:46:38 -0500

    task 6.5 test

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -24,6 +24,7 @@ void hash_table_delete(hash_table_t *ht)
 		{
 			temp = currentnode;
 			free(currentnode->value);
+			free(currentnode->key);
 			free(currentnode);
 			currentnode = temp->next;
 		}
